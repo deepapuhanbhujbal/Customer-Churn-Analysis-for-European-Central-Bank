5504ceb26f579de4734ea43c0c609f57196ec503
DEEPA PUHAN BHUJBAL <71322647+deepapuhanbhujbal@users.noreply.github.com>
2026-09-20 08:19:03 +0530

    Update streamlit_app_european_bank.py

--- streamlit_app_european_bank.py
+++ streamlit_app_european_bank.py
@@ -11,7 +11,7 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
 import os
