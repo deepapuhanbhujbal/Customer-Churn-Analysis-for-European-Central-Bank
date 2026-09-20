4967f9d8bde1f0f13867e42496ca8f5effa97e6f
DEEPA PUHAN BHUJBAL <71322647+deepapuhanbhujbal@users.noreply.github.com>
2026-09-20 08:25:04 +0530

    Update streamlit_app_european_bank.py

--- streamlit_app_european_bank.py
+++ streamlit_app_european_bank.py
@@ -32,7 +32,7 @@ st.set_page_config(page_title="Customer Churn Dashboard", layout="wide")
 
 #Loading the model
 #model=joblib.load("churn_model.pkl'")
-BASE_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "churn_model.pkl")
 
 model = joblib.load(model_path)
