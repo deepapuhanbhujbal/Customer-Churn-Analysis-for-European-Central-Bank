d9ba64c7d308c5161db3f7ee95c94eff84b1cf37
DEEPA PUHAN BHUJBAL <71322647+deepapuhanbhujbal@users.noreply.github.com>
2026-09-20 08:15:28 +0530

    Update streamlit_app_european_bank.py

--- streamlit_app_european_bank.py
+++ streamlit_app_european_bank.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1L8WfQQ2Df_Ngn4i4DuGsJaW-GkK0uqsh
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
