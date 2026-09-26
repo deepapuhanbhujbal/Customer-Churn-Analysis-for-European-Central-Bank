61aac7c999b7a4155d3729bdfec4efa6fe07b1eb
DEEPA PUHAN BHUJBAL <71322647+deepapuhanbhujbal@users.noreply.github.com>
2026-09-26 19:10:58 +0530

    Update streamlit_app_european_bank.py

--- streamlit_app_european_bank.py
+++ streamlit_app_european_bank.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1L8WfQQ2Df_Ngn4i4DuGsJaW-GkK0uqsh
 """
 # pip install matplotlib seaborn
-streamlit
 matplotlib
 pandas
 seaborn
